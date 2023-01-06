83976974f9566811c4d2b6478482ce0abed9d2d1
Jerry Wang <jerry.wang2352@gmail.com>
2023-01-05 21:58:11 -0500

    Write displayWeather

--- script.js
+++ script.js
@@ -1,36 +1,44 @@
-class Weather {
-    constructor(city) {
-    /**
-     * city -> string: city name  
-     * url -> string: Openweather API url  
-     */
-        this.city = city; 
-        this.url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=c42af13053cc1c2a3369e33f927302c8`;
-    }
-    async getWeatherJSON() {
+
+
+async function getWeatherJSON(city) {
     /**
      * Returns the JSON weather file for the city 
-     * Throws 404 error if url is not valid 
+     * Throws error if url is not valid 
      */
-        let response = await fetch(this.url); 
-        if(response.status == 200) {
-            let json = await response.json();
-            console.log(json);
-            return json;   
-        } else {
-           throw new Error("404 Error");  
-        }
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=c42af13053cc1c2a3369e33f927302c8`
+
+    let response = await fetch(url,{mode: 'cors'}); 
+    if(response.ok) {
+        let json = await response.json();
+        return json;   
+    } else {
+       throw new Error(response.statusText);  
     }
 }
 
-function displayWeather() {
+// class display {
+//     constructor(json) {
+//         this.json = json;
+//         this.temp = json.main.temp;  
+//     }
+    
+//     displayWeather() {
+//         alert(this.json.main.temp); 
+//     }
 
+//     displayError() {
+//         return; 
+//     }
+// }
+
+async function displayWeather() {
+    let city = prompt("Enter your city","London"); 
+    try {
+        weatherJSON = await getWeatherJSON(city);
+        alert(weatherJSON.main.temp); 
+    } catch {
+        alert("Cannot find city");
+        displayWeather(); 
+    }
 }
-
-function displayError() {
-
-}
-
-let London = new Weather("London"); 
-console.log(London.url); 
-London.getWeather(); 
+displayWeather(); 
