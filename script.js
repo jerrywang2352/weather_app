e5c2421d62f79b2672b0064bbd36732ac515d1fc
Jerry Wang <jerry.wang2352@gmail.com>
2023-01-11 22:25:46 -0500

    Add daily weather cards styling

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+async function displayWeather(place) {
+    const errorDisplays = document.querySelectorAll(".error"); 
+    try {
+        weatherJSON = await getWeatherJSON(place);
+        console.log(weatherJSON); 
+        displayTemperature(weatherJSON.main.temp,weatherJSON.name,weatherJSON.weather[0].main,weatherJSON.weather[0].icon); 
+        errorDisplays[0].innerHTML = '';
+        errorDisplays[1].innerHTML = '';
+    } catch {
+        errorDisplays[0].innerHTML = "Location not found.";
+        errorDisplays[1].innerHTML = 'Search must be in the form of "City", "City, State" or "City, Country".';
+    }
+}
+
 async function getWeatherJSON(city) {
     /**
      * Returns the JSON weather file for the city 
@@ -14,23 +28,8 @@ async function getWeatherJSON(city) {
 }
 
 
-
-async function displayWeather(place) {
-    const errorDisplays = document.querySelectorAll(".error"); 
-    try {
-        weatherJSON = await getWeatherJSON(place);
-        console.log(weatherJSON); 
-        displayTemperature(weatherJSON.main.temp,weatherJSON.name,weatherJSON.weather[0].main,weatherJSON.weather[0].icon); 
-        errorDisplays[0].innerHTML = '';
-        errorDisplays[1].innerHTML = '';
-    } catch {
-        errorDisplays[0].innerHTML = "Location not found.";
-        errorDisplays[1].innerHTML = 'Search must be in the form of "City", "City, State" or "City, Country".';
-    }
-}
-
-
 function displayTemperature(temp,city,precipitation,iconID) {
+    //display current temperature
     const tempDisplay = document.querySelector(".temperature");
     const precipitationDisplay = document.querySelector(".precipitation");
     const cityDisplay = document.querySelector(".city");
@@ -40,13 +39,24 @@ function displayTemperature(temp,city,precipitation,iconID) {
     precipitationDisplay.innerHTML = precipitation;
     console.log(iconID);
     const iconURL = `https://openweathermap.org/img/wn/${iconID}@2x.png`;
-    iconDisplay.src = iconURL; 
+    iconDisplay.src = iconURL;
+    
+    //display forecast
+    const forecastOne = document.getElementById('one');  
+    const forecastTwo = document.getElementById('two'); 
+    const forecastThree = document.getElementById('three'); 
+    const forecastFour = document.getElementById('four'); 
+    const forecastFive= document.getElementById('five'); 
+    const forecastSix= document.getElementById('six'); 
+    const forecastSeven = document.getElementById('seven'); 
 }
 
 function kelvinToF(kelvin) {
     return Math.round(1.8*(kelvin-273) + 32);
 }
 
+
+//event listener for location input 
 const input = document.querySelector('input');
 input.addEventListener('keypress', e => {
     if (e.key === "Enter") {
